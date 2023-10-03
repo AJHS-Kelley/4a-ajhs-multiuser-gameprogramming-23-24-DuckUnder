3dfd58ec520f938e64d06ada2f3fc76994c2a8f3
DuckUnder <111576267+DuckUnder@users.noreply.github.com>
2023-10-03 13:38:58 -0400

    v0.0
    Added program info

---
+++ multiUserGameProgramming/computer_science_exercises/02_Collections/programTemplate.cs
@@ -0,0 +1,13 @@
+//Amos Rains, 02_Collections, v0.0
+using System;
+
+namespace UPDATEFOREACHPROGRAM 
+{
+    class UPDATEFOREACHPROGRAM
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }   
+}
