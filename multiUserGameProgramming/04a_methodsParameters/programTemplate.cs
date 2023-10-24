4f1bc5b269cb34c752f8b3aeb2ecca38ad962c3d
DuckUnder <amosrains736@gmail.com>
2023-10-24 14:41:49 -0400

    V0.2 Methods Parameters
    Added DoubleUp and MakePancakes

--- multiUserGameProgramming/04a_methodsParameters/programTemplate.cs
+++ multiUserGameProgramming/04a_methodsParameters/programTemplate.cs
@@ -1,4 +1,4 @@
-// Amos Rains, Methods Parameters, v0.3
+// Amos Rains, Methods Parameters, v0.2
 using System;
 
 namespace MethodsParameters
@@ -12,9 +12,36 @@ namespace MethodsParameters
         {
             Console.WriteLine("I like mine with peperoni, olives, and lots of cheese.\n");
         }
+        // static -- This method belongs to the current class, it is not a object.
+        // void -- This method has no return value. 
+
+        static int DoubleUp()
+        {
+            int sum = 0; 
+            Console.WriteLine("This method will double a number and return it.\n");
+            Console.WriteLine("Please enther a number on the next line.\n");
+            sum = System.Convert.ToInt32(Console.ReadLine());
+            //sum = sum * 2;
+            //Console.WriteLine(sum);
+            return sum;
+        }
+
+        // Methods with Parameters
+        
+        static void MakePancakes(int num)
+        {
+            for (int i = 0; i < num; i++) 
+            {
+                Console.WriteLine("One golden, fluffy pancake coming up!\n");
+            }
+        }
+
+
         static void Main(string[] args)
         {
-            myMethod();
+            //myMethod();
+            //DoubleUp();
+            MakePancakes(10);
         }
     }   
 }
