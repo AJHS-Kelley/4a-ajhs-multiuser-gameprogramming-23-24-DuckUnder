bb96d9d80f761d3dbc73313be33a2594b0219a11
DuckUnder <amosrains736@gmail.com>
2023-10-24 14:11:28 -0400

    Methods

---
+++ multiUserGameProgramming/04a_methodsParameters/programTemplate.cs
@@ -0,0 +1,20 @@
+// Amos Rains, Methods Parameters, v0.3
+using System;
+
+namespace MethodsParameters
+{
+    class MethodsParameters
+    {
+        // METHOD -- named block of code, can be used over and over. 
+        // All methods have a SIGNATURE  that defines their name, parameters, and output. 
+        // Example signature
+        static void myMethod()
+        {
+            Console.WriteLine("I like mine with peperoni, olives, and lots of cheese.\n");
+        }
+        static void Main(string[] args)
+        {
+            myMethod();
+        }
+    }   
+}
