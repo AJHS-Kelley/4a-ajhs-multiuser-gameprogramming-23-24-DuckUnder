fee11468fdc03f5e2b835be79253703693608ff5
DuckUnder <amosrains736@gmail.com>
2023-11-14 13:44:55 -0500

    v0.0
    Start of program.

---
+++ multiUserGameProgramming/05a_exampleGameMethods/exampleGameMethods.cs
@@ -0,0 +1,13 @@
+// Amos Rains, Example Game Methods, v0.0
+using System;
+
+namespace exampleGameMethods
+{
+    class exampleGameMethods
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }   
+}
