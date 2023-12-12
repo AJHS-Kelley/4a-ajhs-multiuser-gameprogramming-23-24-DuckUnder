137fd6f41d8ad32b82a1885a053c13e349b943d7
DuckUnder <amosrains736@gmail.com>
2023-12-11 20:11:00 -0500

    Update exampleGameMethods.cs

--- multiUserGameProgramming/05a_exampleGameMethods/exampleGameMethods.cs
+++ multiUserGameProgramming/05a_exampleGameMethods/exampleGameMethods.cs
@@ -1,34 +1,31 @@
 // Amos Rains, Example Game Methods, v0.3
 using System;
+using System.Linq;
 
 namespace exampleGameMethods
 {
     class exampleGameMethods
     {
-        static string calloutsMethod()
+        static string commandsMethod()
         {
-            string[] callouts = {"pass", "ball", "shoot"};
-            bool valid = false;
-            string playerCallout = Console.ReadLine().ToLower();
-            bool commandGot = false;
-            while (commandGot == false)
+            string[] commands = {"pass", "ball", "shoot"};
+            string playerInput = Console.ReadLine().ToLower();
+            while (true)
             {
-                for (int i = 0; i < callouts.length; i++)
+                if (commands.Any(playerInput.Contains) == true)
                 {
-                    if (playerCallout == callouts[i])
-                    {
-                        commandGot = true;
-                    } else if (i == callouts.length)
-                    {
-                        Console.WriteLine("That's not a command?? Try again!");
-                        playerCallout = Console.ReadLine().ToLower();
-                    }
+                    break;
+                } else
+                {
+                    Console.WriteLine("That's not a command?? Try again!");
+                    playerInput = Console.ReadLine().ToLower();
                 }
             }
-
-
-
-            return playerCallout;
+            return playerInput;
+        }
+        static void passBall()
+        {
+            
         }
         static bool ballInHands()
         {
@@ -45,26 +42,37 @@ namespace exampleGameMethods
             return playerWithBall;
         }
 
-        static void getBall()
-        {
-        string[] passingCallOuts = { "Ball!", "Pass!", "Rock Me!", "Hand Here!" };
-            if (ballInHands() == false && Array.Exists(passingCallOuts, call => call == Console.ReadLine()))
-            {
-                Console.WriteLine("You have finally gotten the ball in your hands. What are you gonna do with it?\n");
-            }
-            else if (ballInHands() == true && Array.Exists(passingCallOuts, call => call == Console.ReadLine()))
-            {
-                Console.WriteLine("You already have the ball?? Why are you asking for the Ball you weirdo??");
-            }
-            else
-            {
-                Console.WriteLine("What are you waiting for?? DO SOMETHING????");
-            }
-        }
+        // static void getBall()
+        // {
+        // string[] passingCallOuts = { "Ball!", "Pass!", "Rock Me!", "Hand Here!" };
+        //     if (ballInHands() == false && Array.Exists(passingCallOuts, call >= call == Console.ReadLine()))
+        //     {
+        //         Console.WriteLine("You have finally gotten the ball in your hands. What are you gonna do with it?\n");
+        //     }
+        //     else if (ballInHands() == true && Array.Exists(passingCallOuts, call >= call == Console.ReadLine()))
+        //     {
+        //         Console.WriteLine("You already have the ball?? Why are you asking for the Ball you weirdo??");
+        //     }
+        //     else
+        //     {
+        //         Console.WriteLine("What are you waiting for?? DO SOMETHING????");
+        //     }
+        // }
 
         static void Main(string[] args)
         {
-            getBall();
+            // getBall();
+            commandsMethod();
+            // string test = "pass";
+            // string[] things = {"pass", "lol", "dumb"};
+            // // Console.WriteLine(things.Any(test.Contains));
+            // bool b = things.Any(test.Contains);
+            // Console.WriteLine(b);
+
+            while (true)
+            {
+
+            }
         }
     }
 }
