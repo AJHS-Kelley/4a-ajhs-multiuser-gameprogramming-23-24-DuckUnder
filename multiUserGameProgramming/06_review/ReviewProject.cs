f50d770b00a3f23367d0efd3ddb84607f84d6579
DuckUnder <amosrains736@gmail.com>
2024-01-10 14:06:07 -0500

    v0.1 Longs, Characters, & Doubles
    Learned how to do Longs, Characters, and Doubles

---
+++ multiUserGameProgramming/06_review/ReviewProject.cs
@@ -0,0 +1,48 @@
+// Amos Rains, ReviewProject, v0.0
+using System;
+
+namespace ReviewProject
+{
+    class ReviewProject
+    {
+        static void Main(string[] args)
+        {
+            
+            // Declaring and Assigning Variables 
+            // Integer -- In C# the Max range for Intergers is -2,147,483,648 to 2,147,483,648
+            int sum = 0;
+
+            // Long -- -9,223,372,036,854,775,808 to 9,223,372,036,854,775,808
+            // Long = 8 Bytes 
+            long myLong = 1928742747247242904;
+            // String 
+            // 2-bytes PER CHARACTER
+            // Must be in DOUBLE QUOTES " "
+
+
+            string dadName = "Amos Rains III";
+
+            // Character (Char)
+            // Single character, in SINGLE QUOTES ' '
+            // 2-Bytes 
+            char myChar = 'M';
+
+            // Boolean -- true / false
+            // Boolean = 1 bit 
+
+
+            bool result = (x > y) && (x == 10);
+            //Float -- Stores up to 6-7 decimal digits. 
+
+            float GPA = 3.79f;
+
+            // Double -- Stores up to 15 decimal digits. 
+            // Double = 8 Bytes 
+            double myDouble = 9.12345678910234D;
+        
+
+
+
+        }
+    }   
+}
