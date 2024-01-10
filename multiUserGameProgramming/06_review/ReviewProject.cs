b197b23f8ae28a1320731d7fd35c4ca8d6d219ec
DuckUnder <amosrains736@gmail.com>
2024-01-10 14:48:44 -0500

    v1.0
    Added Cases, Converts, explained IMPLICT and EXPLICIT

--- multiUserGameProgramming/06_review/ReviewProject.cs
+++ multiUserGameProgramming/06_review/ReviewProject.cs
@@ -1,4 +1,4 @@
-// Amos Rains, ReviewProject, v0.0
+// Amos Rains, ReviewProject, v1.0
 using System;
 
 namespace ReviewProject
@@ -31,7 +31,7 @@ namespace ReviewProject
             // Boolean = 1 bit 
 
 
-            bool result = (x > y) && (x == 10);
+            bool result = false;
             //Float -- Stores up to 6-7 decimal digits. 
 
             float GPA = 3.79f;
@@ -39,6 +39,63 @@ namespace ReviewProject
             // Double -- Stores up to 15 decimal digits. 
             // Double = 8 Bytes 
             double myDouble = 9.12345678910234D;
+
+            // Type Casting -- changing one date type to another.
+            // IMPLICIT -- happens automatically when going smaller -> larger
+            // char -> int -> long -> float -> double 
+
+            int myInt = 9;
+            double thatDouble = myInt;
+            Console.WriteLine(thatDouble);
+            // EXPLICIT -- Must be done manually when going larger -> smaller 
+            // double -> float -> long -> int -> char 
+            double exampleDouble = 9.1234567890D;
+            int theInt = (int) thatDouble;
+
+            // Conversion Methods 
+            // Convert.ToString() // String 
+            string freeString = "Free the People";
+        
+            // Convert.ToDouble()  // Double
+            double canDouble = 3.19412445455434D;
+
+            // Convert.ToInt32()  // Interger
+            int danInt = 555;
+
+            // Convert.ToInt64()  // Long
+            int fullLong = 1234;
+
+            // Switch Statement
+            int dayOfWeek = 5;
+            switch (dayOfWeek)
+            {
+                case 1:
+                    Console.WriteLine("The Day is Sunday.\n");
+                    break;
+                case 2:
+                    Console.WriteLine("The Day is Monday.\n");
+                    break;
+                case 3:
+                    Console.WriteLine("The Day is Tuesday.\n");
+                    break;
+                case 4:
+                    Console.WriteLine("The Day is Wednesday.\n");
+                    break;
+                case 5:
+                    Console.WriteLine("The Day is Thursday.\n");
+                    break;
+                case 6:
+                    Console.WriteLine("The Day is Friday.\n");
+                    break;
+                case 7:
+                    Console.WriteLine("The Day is Saturday.\n");
+                    break;
+                default:
+                    Console.WriteLine("No Match found.\n");
+                    break;
+            }
+
+
         
 
 
